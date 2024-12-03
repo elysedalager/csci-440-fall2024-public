66e1427d2e2a463f4b1b799a2c100642ea9ab4a8
elysedalager <elysemdalager@gmail.com>
2024-12-02 22:38:12 -0800

    Employees updates

--- src/main/java/edu/montana/csci/csci440/helpers/EmployeeHelper.java
+++ src/main/java/edu/montana/csci/csci440/helpers/EmployeeHelper.java
@@ -2,10 +2,7 @@ package edu.montana.csci.csci440.helpers;
 
 import edu.montana.csci.csci440.model.Employee;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class EmployeeHelper {
 
@@ -14,17 +11,16 @@ public class EmployeeHelper {
         Employee boss = Employee.find(1); // root boss
         // and use this data structure to maintain reference information needed to build the tree structure
         Map<Long, List<Employee>> employeeMap = new HashMap<>();
+
         List<Employee> all = Employee.all();
         for (Employee emp : all) {
-            List<Employee> employees = employeeMap.get(emp.getReportsTo());
-            if (employees == null) {
-                employees = new ArrayList<>();
-                employeeMap.put(emp.getEmployeeId(), employees);
-            }
-            employees.add(emp);
+            long reports = emp.getReportsTo();
+            List<Employee> reportsTo = (employeeMap.get(reports) != null) ? employeeMap.get(reports) : new LinkedList<>();
+            employeeMap.putIfAbsent(emp.getReportsTo(), reportsTo);
+            reportsTo.add(emp);
         }
 
-        return "<ul>" + makeTree(boss, employeeMap) + "</ul>";
+        return "<ul>" + makeTree(Objects.requireNonNull(boss), employeeMap) + "</ul>";
     }
 
     // TODO - currently this method just uses the employee.getReports() function, which
@@ -32,9 +28,11 @@ public class EmployeeHelper {
     public static String makeTree(Employee employee, Map<Long, List<Employee>> employeeMap) {
         String list = "<li><a href='/employees/" + employee.getEmployeeId() + "'>"
                 + employee.getEmail() + "</a><ul>";
-        List<Employee> reports = employee.getReports();
-        for (Employee report : reports) {
-            list += makeTree(report, employeeMap);
+        List<Employee> reports = employeeMap.get(employee.getEmployeeId());
+        if (reports != null){
+            for (Employee report : reports) {
+                list += makeTree(report, employeeMap);
+            }
         }
         return list + "</ul></li>";
     }
